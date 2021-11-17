86e79a71971c6ccd793283ab9f5ab60a22dd7f21
SREERAJVAPPALA <93855354+SREERAJVAPPALA@users.noreply.github.com>
2021-11-17 10:39:30 +0530

    17/11/2021

---
+++ area.py
@@ -0,0 +1,4 @@
+r=input("enter the radius")
+r=float(r)
+import math
+print("area of the circle is ",math.pi*r*r)
---
+++ temp.py
@@ -0,0 +1,3 @@
+c=int(input("enter the temperature in celsius"))
+f=(c*(9/5)+32)
+print("fahrenheit value is ",f)
